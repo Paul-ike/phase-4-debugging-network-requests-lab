91b437c6785542bb8c4591236fa1ba5aa1114a14
Paul <paul.ngugi@student.moringaschool.com>
2023-03-17 09:16:14 +0300

    done

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,5 +1,7 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def index
     toys = Toy.all
@@ -12,13 +14,13 @@ class ToysController < ApplicationController
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.update(toy_params)
     render json: toy
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.destroy
     head :no_content
   end
@@ -29,4 +31,15 @@ class ToysController < ApplicationController
     params.permit(:name, :image, :likes)
   end
 
+  def find_toy
+    Toy.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Toy not found" }, status: :not_found
+  end
+
+  # def render_unprocessable_entity_response(invalid)
+  #   render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  # end
 end
--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,5 +1,5 @@
 class Toy < ApplicationRecord
-    # validates :name, presence: true
-    # validates :image, presence: true
-    # validates :image, presence: true
+    # validates :name, presence: true, uniqueness: true
+    # validates :image, presence: true, uniqueness: true
+    # validates :likes, presence: true, numericality: { only_integer: true }
 end
